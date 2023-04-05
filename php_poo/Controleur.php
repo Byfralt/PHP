6a799079b1231064cd581eb35e4aacad43d41efb
CORVAISIER Kyllian <kyllian.corvaisier@GB42.local>
2023-04-05 10:22:12 +0200

    v2

--- php_poo/Controleur.php
+++ php_poo/Controleur.php
@@ -1,44 +1,35 @@
 <?php
-<<<<<<< HEAD
-class PersonnageControleur{
-    public $tabPerso = array();
-    public function __construct($this,$nom,$pv,$type,$force,$niveau){
-        
-=======
     class PersonnageControleur{
         public $tabPerso = array();
-        public function __construct($nom,$pv,$type,$force,$niveau){
-            $perso = new Personnage($nom,$pv,$type,$force,$niveau);
-            // $perso = new Personnage('bob',100,"eau",40,3);
-        }
+        public function __construct($this,$nom,$pv,$type,$force,$niveau){
+        class PersonnageControleur{
+            public $tabPerso = array();
+            public function __construct($nom,$pv,$type,$force,$niveau){
+                $perso = new Personnage($nom,$pv,$type,$force,$niveau);
+                // $perso = new Personnage('bob',100,"eau",40,3);
+            }
 
-        public function creerPerso(){
-            $perso = array(
-                'nom' => $nom,
-                'pv' => $pv,
-                'type' => $type,
-                'force' => $force,
-                'niveau' => $niveau,
-                echo "toto"
-            );
-            array_push($this -> tabPerso,$perso);
-        }
+            public function creerPerso(){
+                $perso = array(
+                    'nom' => $nom,
+                    'pv' => $pv,
+                    'type' => $type,
+                    'force' => $force,
+                    'niveau' => $niveau,
+                    echo "toto"
+                );
+                array_push($this -> tabPerso,$perso);
+            }
 
-        public function Afficher(){
-            foreach($this->tabPerso as $perso)
-                echo "Nom: ".$perso['nom']."<br>";
-                echo "Pv: ".$perso['pv']."<br>";
-                echo "Type: ".$perso['type']."<br>";
-                echo "Force: ".$perso['force']."<br>";
-                echo "Niveau: ".$perso['niveau']."<br>";
+            public function Afficher(){
+                foreach($this->tabPerso as $perso)
+                    echo "Nom: ".$perso['nom']."<br>";
+                    echo "Pv: ".$perso['pv']."<br>";
+                    echo "Type: ".$perso['type']."<br>";
+                    echo "Force: ".$perso['force']."<br>";
+                    echo "Niveau: ".$perso['niveau']."<br>";
+                }
         }
->>>>>>> 418f95b14836f14897872e5b649692a3222b9a8e
+        }
     }
-    public function creerPerso(){
-        echo "toto"
-    }
-    public function Afficher(){
-
-    }
-}
 ?>
